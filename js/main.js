a6709835a6b9eb7dd2b154c05760ecfcd66d4314
MuhamedElsafy <muhamedd.kh@gmail.com>
2023-04-30 16:49:15 +0300

    task1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Muhammed', 'FrontEnd Developer.',],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop:true,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+  });
