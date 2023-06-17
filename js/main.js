aa3ad6408e343f77b5abd50a6c1e965a345da2ec
MohammedYa <mohamedel3sawy4110@gmail.com>
2023-06-17 23:10:44 +0300

    add option color

--- js/main.js
+++ js/main.js
@@ -24,3 +24,22 @@ var typed = new Typed('#element', {
     $("#list"). slideToggle(2000);
   })
 
+  $("#setting").click(function(){
+    let optionWidth=$(".switches").outerWidth() 
+    let right=$(".option").css("right")
+    if(right=="0px"){
+      $(".option").animate({"right":-optionWidth},1500)
+    }
+    else{
+      $(".option").animate({"right":"0px"},1500)
+    }
+  })
+
+
+
+
+
+  let bgOptionColor=["#20c997","#6610f2","#007bff","#17a2b8","#dc3545","#e83e8c","#28a745","#ffc107","#fd7e14","#795548"]
+for(let i=0;i<bgOptionColor.length;i++){
+  $(".color").eq(i).css('backgroundColor',bgOptionColor[i])
+}
